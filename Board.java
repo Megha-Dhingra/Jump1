5cbcb069cbd089687bc264b5224a6162c602acb0
Megha-Dhingra <39176611+Megha-Dhingra@users.noreply.github.com>
2018-05-10 18:50:29 -0700

    Add files via upload

---
+++ Board.java
@@ -0,0 +1,15 @@
+//import java.awt.Graphics;
+//
+//public class Board {
+//	//private Snake snake;
+//	private RedThing red; 
+//	public Board( ) {
+//		//snake = new Snake();
+//		red = new RedThing(); 
+//	}
+//
+//	public void draw (Graphics g) {
+//		snake.draw(g);
+//		red.draw(g); 
+//	}
+//}
